150b123862e7af0d01204d1d0ecbe3ab16b0b022
qingrongdys <105169533+qingrongdys@users.noreply.github.com>
2024-09-16 10:30:27 -0400

    Add Excel file and main.cpp update

--- MTH9821/hw/hw2/hw2_cpp/main.cpp
+++ MTH9821/hw/hw2/hw2_cpp/main.cpp
@@ -5,11 +5,12 @@
 #include <cmath>
 #include <boost/math/distributions/normal.hpp>
 #include <numeric>
+#include <algorithm> 
 
 int main()
 {
-    double K = 54;
-    double S0 = 56;
+    double K = 54.0;
+    double S0 = 56.0;
     double T = 9.0 / 12.0;
     double r = 0.02;
     double q = 0.0;
@@ -90,7 +91,7 @@ int main()
         std::vector<int> n;
         for (int i = 0; i < 10; ++i)
         {
-            n.push_back(10000 * (1 << i)); // 1 << i 相当于 2^i
+            n.push_back(10000 * (1 << i)); // 1 << i equals 2^i
         }
 
         std::vector<double> V_AV_n_hat_list;
@@ -139,26 +140,15 @@ int main()
                       << std::endl;
         }
     }
-    std::cout << "\n---Basket Options---\n" << std::endl;
-    { // Basket Options
+    { // Moment Matching Technique
         std::vector<int> n;
-        double K = 50;
-        double S10 = 26;
-        double S20 = 29;
-        double T = 0.5;
-        double r = 0.025;
-        double q = 0.0;
-        double sigma1 = 0.31;
-        double sigma2 = 0.21;
-        double rho = 0.3;
-
-        for (int i = 0; i < 9; ++i)
+        for (int i = 0; i < 10; ++i)
         {
-            n.push_back(10000 * (1 << i)); // 
+            n.push_back(10000 * (1 << i)); // 1 << i equals 2^i
         }
 
-        std::vector<double> V_n_hat_list;
-        //std::vector<double> error_list;
+        std::vector<double> V_MM_n_hat_list;
+        std::vector<double> MM_error_list;
 
         auto uniform_generator = LinearCongruentialGenerator();
         auto normal_generator = MarsagliaBrayNormalGenerator(uniform_generator);
@@ -170,100 +160,114 @@ int main()
 
             for (int i = 0; i < nn; ++i)
             {
-                double zi1 = normal_generator.generate();
-                double zi2 = normal_generator.generate();
+                double zi = normal_generator.generate();
 
-                // 计算Si1和Si2
-                double Si1 = S10 * std::exp((r - 0.5 * sigma1 * sigma1) * T + sigma1 * std::sqrt(T) * zi1);
-                double Si2 = S20 * std::exp((r - 0.5 * sigma2 * sigma2) * T + sigma2 * std::sqrt(T) * (rho * zi1 + std::sqrt(1-rho*rho) * zi2));
-
-                Si_list.push_back(Si1+Si2);
-
-                // 计算Vi1和Vi2
-                Vi_list.push_back(std::exp(-r * T) * std::max(Si1+Si2-K, 0.0));
+                // Calculate Si
+                double Si = S0 * std::exp((r - 0.5 * sigma * sigma) * T + sigma * std::sqrt(T) * zi);
+                Si_list.push_back(Si);
             }
 
-            // 计算 V_AV_n_hat
-            double V_n_hat = std::accumulate(Vi_list.begin(), Vi_list.end(), 0.0) / Vi_list.size();
-            V_n_hat_list.push_back(V_n_hat);
+            // Calculate mean value S_hat
+            double S_hat = std::accumulate(Si_list.begin(), Si_list.end(), 0.0) / nn;
+            double Si_tilde;
 
-            // 计算误差
-            //double error = std::abs(V_n_hat - put_res["put_price"]);
-            //error_list.push_back(error);
+            for (int i = 0; i < nn; ++i)
+            {
+                // Calculate Si_tilde
+                Si_tilde = Si_list[i] * (std::exp(r * T) * S0 / S_hat);
 
-            // 输出结果
+                // Calculate Vi_tilde
+                Vi_list.push_back(std::exp(-r * T) * std::max(K - Si_tilde, 0.0));
+            }
+
+            // Calculate V_MM_n_hat
+            double V_MM_n_hat = std::accumulate(Vi_list.begin(), Vi_list.end(), 0.0) / nn;
+            V_MM_n_hat_list.push_back(V_MM_n_hat);
+
+            // Calculate error
+            double error = std::abs(V_MM_n_hat - put_res["put_price"]);
+            MM_error_list.push_back(error);
+
+            // Output result
             std::cout << "n: " << nn
-                      << " V_n_hat: " << V_n_hat
-                      //<< " error: " << error
-                      << std::endl;
+                << " V_MM_n_hat: " << V_MM_n_hat
+                << " error: " << error
+                << std::endl;
         }
     }
 
-    std::cout << "\n---Basket Options Path Dependent---\n" << std::endl;
-    { // Basket Options Path Dependent
-        std::vector<int> N;
-        double K = 50;
-        double S10 = 26;
-        double S20 = 29;
-        double T = 0.5;
-        double r = 0.025;
-        double q = 0.0;
-        double sigma1 = 0.31;
-        double sigma2 = 0.21;
-        double rho = 0.3;
-        double m = 150;
-        double n = 50;
-        double dt = T/m;
-        std::cout << "dt: " << dt << std::endl;
-
+    { // Simultaneous Moment Matching and Control Variate Technique
+        std::vector<int> n;
         for (int i = 0; i < 10; ++i)
         {
-            N.push_back(n * (1 << i)); // 
+            n.push_back(100000 * (1 << i)); // 1 << i equals 2^i
         }
 
-        std::vector<double> V_n_hat_list;
+        std::vector<double> V_CV_MM_n_hat_list;
+        std::vector<double> CV_MM_error_list;
 
         auto uniform_generator = LinearCongruentialGenerator();
         auto normal_generator = MarsagliaBrayNormalGenerator(uniform_generator);
 
-        for (auto nn : N)
+        for (auto nn : n)
         {
-            std::vector<double> Si_list;
-            std::vector<double> Vi_list;
+            std::vector<double> Si_tilde_list(nn);
+            for (int i = 0; i < nn; ++i)
+            {
+                double zi = normal_generator.generate();
+                double Si = S0 * std::exp((r - 0.5 * sigma * sigma) * T + sigma * std::sqrt(T) * zi);
+                Si_tilde_list[i] = Si;
+            }
+
+            // calculate S_hat, moment matching
+            double S_hat = std::accumulate(Si_tilde_list.begin(), Si_tilde_list.end(), 0.0) / nn;
+            std::vector<double> Vi_tilde_list(nn);
 
             for (int i = 0; i < nn; ++i)
             {
-                double Si1 = S10;
-                double Si2 = S20;
-                for (int j = 0; j < m; ++j)
-                {
-                    double zi1 = normal_generator.generate();
-                    double zi2 = normal_generator.generate();
+                double Si_tilde = Si_tilde_list[i] * (std::exp(r * T) * S0 / S_hat);
+                Si_tilde_list[i] = Si_tilde;
 
-                    // 计算Si1和Si2
-                    Si1 = Si1 * std::exp((r - 0.5 * sigma1 * sigma1) * dt + sigma1 * std::sqrt(dt) * zi1);
-                    Si2 = Si2 * std::exp((r - 0.5 * sigma2 * sigma2) * dt + sigma2 * std::sqrt(dt) * (rho * zi1 + std::sqrt(1-rho*rho) * zi2));
-                }
-                
-
-                Si_list.push_back(Si1+Si2);
-
-                // 计算Vi1和Vi2
-                Vi_list.push_back(std::exp(-r * T) * std::max(Si1+Si2-K, 0.0));
+                // calculate Vi_tilde
+                Vi_tilde_list[i] = std::exp(-r * T) * std::max(K - Si_tilde, 0.0);
             }
 
-            // 计算 V_AV_n_hat
-            double V_n_hat = std::accumulate(Vi_list.begin(), Vi_list.end(), 0.0) / Vi_list.size();
-            V_n_hat_list.push_back(V_n_hat);
-            std::cout << "Vi_list1: " << Vi_list[0] << std::endl;
-            std::cout << "Vi_list2: " << Vi_list[1] << std::endl;
-            std::cout << "Si_list1: " << Si_list[0] << std::endl;
-            std::cout << "Si_list2: " << Si_list[1] << std::endl;
-            // 输出结果
+            // calculate control variate coefficient b_hat
+            double Si_mean = std::accumulate(Si_tilde_list.begin(), Si_tilde_list.end(), 0.0) / nn;
+            double Vi_mean = std::accumulate(Vi_tilde_list.begin(), Vi_tilde_list.end(), 0.0) / nn;
+
+            double cov_Si_Vi = 0.0;
+            double var_Si = 0.0;
+            for (int i = 0; i < nn; ++i)
+            {
+                cov_Si_Vi += (Si_tilde_list[i] - Si_mean) * (Vi_tilde_list[i] - Vi_mean);
+                var_Si += (Si_tilde_list[i] - Si_mean) * (Si_tilde_list[i] - Si_mean);
+            }
+            cov_Si_Vi /= (nn - 1);
+            var_Si /= (nn - 1);
+
+            double b_hat = cov_Si_Vi / var_Si;
+
+            std::vector<double> Wi_list(nn);
+
+            for (int i = 0; i < nn; ++i)
+            {
+                double Wi = Vi_tilde_list[i] - b_hat * (Si_tilde_list[i] - std::exp(r * T) * S0);
+                Wi_list[i] = Wi;
+            }
+
+            double V_CV_MM_n_hat = std::accumulate(Wi_list.begin(), Wi_list.end(), 0.0) / nn;
+            V_CV_MM_n_hat_list.push_back(V_CV_MM_n_hat);
+
+            // calculate error
+            double error = std::abs(V_CV_MM_n_hat - put_res["put_price"]);
+            CV_MM_error_list.push_back(error);
+
+            // Output results
             std::cout << "n: " << nn
-                      << " V_n_hat: " << V_n_hat
-                      //<< " error: " << error
-                      << std::endl;
+                << " V_CV_MM_n_hat: " << V_CV_MM_n_hat
+                << " error: " << error
+                << std::endl;
         }
     }
 
